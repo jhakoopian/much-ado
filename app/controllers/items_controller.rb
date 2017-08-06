8aca75f30bb5961018eca1327e7ff17a63fa667a
Joseph Hakoopian <joseph.hakoopian@gmail.com>
2017-08-06 10:55:09 -0400

    mark to-do items as complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,24 @@
 class ItemsController < ApplicationController
-  def index
-    @items = current_user.items
-  end
-
-  def new
-    @item = Item.new
-  end
-
   def create
     @item = current_user.items.new(item_params)
 
     if @item.save
       flash[:notice] = "To-do item was saved."
-      redirect_to items_path
     else
-      flash.now[:alert] = "There was an error saving the to-do item. Please try again."
-      render :new
+      flash[:alert] = "There was an error saving the to-do item. Please try again."
     end
+    redirect_to current_user
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+
+    if @item.destroy
+      flash[:notice] = "\"#{@item.name}\" was successfully removed from to-do list."
+    else
+      flash[:alert] = "There was an error removing this item from your to-do list."
+    end
+    redirect_to current_user
   end
 
   private
